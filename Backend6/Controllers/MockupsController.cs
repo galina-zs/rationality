eff14cd8d1f9451565d11b02c348e157b531617e
galina-zs <zab.galina.24@gmail.com>
2021-06-26 18:06:40 +0300

    init .NET MVC. Based 6 backend lab

---
+++ Backend6/Controllers/MockupsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rationality.Controllers
+{
+    public class MockupsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult AllForums()
+        {
+            return this.View();
+        }
+
+        public IActionResult SingleForum()
+        {
+            return this.View();
+        }
+
+        public IActionResult SingleTopic()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Backend6/Services/IUserPermissionsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Rationality.Models;
+
+namespace Rationality.Services
+{
+    public interface IUserPermissionsService
+    {
+        Boolean CanEditPost(Post post);
+
+        Boolean CanEditPostComment(PostComment postComment);
+    }
+}
