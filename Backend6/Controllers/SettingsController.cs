ca6646cb368aad6db247500fb92ab4136e1065b3
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2021-06-29 00:55:32 +0700

    -Added Settings

--- Backend6/Controllers/SettingsController.cs
+++ Backend6/Controllers/SettingsController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Rationality.Data;
+using Rationality.Models;
+using Rationality.Models.ViewModels;
+using Rationality.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +14,31 @@ namespace Rationality.Controllers
 {
     public class SettingsController : Controller
     {
-        // GET: SettingsController
-        public ActionResult Index()
+        private readonly ApplicationDbContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IUserPermissionsService userPermissions;
+
+        public SettingsController (ApplicationDbContext context, UserManager<ApplicationUser> userManager, IUserPermissionsService userPermissions)
         {
-            return View();
+            this.context = context;
+            this.userManager = userManager;
+            this.userPermissions = userPermissions;
+        }
+        // GET: SettingsController
+        public async Task<IActionResult> Index()
+        {
+            var user = await this.userManager.GetUserAsync(this.HttpContext.User);
+            var model = new SettingsViewModel { 
+                UserName = user.UserName,
+                Email = user.Email,
+                Age = user.Age,
+                Weight = user.Weight,
+                Height = user.Height,
+                Goal = user.Goal,
+                PhysicalActivity = user.Activity,
+                MoneyPerWeek = user.MoneyPerMonth
+            };
+            return View(model);
         }
 
         // GET: SettingsController/Details/5
@@ -30,16 +56,22 @@ namespace Rationality.Controllers
         // POST: SettingsController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> UpdateInfo(SettingsViewModel model)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction(nameof(Index));
+                var user = await this.userManager.GetUserAsync(this.HttpContext.User);
+                user.Gender = model.Gender;
+                user.Email = model.Email;
+                user.Activity = model.PhysicalActivity;
+                user.Age = model.Age;
+                user.Height = model.Height;
+                user.Weight = model.Weight;
+                user.Goal = model.Goal;
+                user.MoneyPerMonth = model.MoneyPerWeek;
+                context.SaveChanges();
             }
-            catch
-            {
-                return View();
-            }
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: SettingsController/Edit/5
---
+++ Backend6/Models/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rationality.Models.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public Goal Goal { get; set; }
+        public PhysicalActivity PhysicalActivity { get; set; }
+        public int MoneyPerWeek { get; set; }
+    }
+}
