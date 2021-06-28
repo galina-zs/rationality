adc1051b919ca12c1d5d0dfd1c65f67521fa1909
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2021-06-29 03:01:47 +0700

    Добавил несколько недоделанных сервисов для дня

---
+++ Backend6/Services/GenerateWeekService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rationality.Data;
+using Rationality.Models;
+using Rationality.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rationality.Services
+{
+    public class GenerateWeekService : IGenerateWeekService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenerateWeekService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void GenerateWeek(ApplicationUser user, Nutrition userNutrition)
+        {
+            int mediumPriceForMeal = (user.MoneyPerMonth / 7) / 3;
+            var recipes = _context.Recipes
+                .SingleOrDefaultAsync(m => m.Meal == Meal.Breakfast && m.Price < mediumPriceForMeal);
+            
+
+
+        }
+    }
+}
---
+++ Backend6/Services/IGenerateWeekService.cs
@@ -0,0 +1,14 @@
+﻿using Rationality.Models;
+using Rationality.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rationality.Services
+{
+    public interface IGenerateWeekService
+    {
+        void GenerateWeek(ApplicationUser user, Nutrition userNutrition);
+    }
+}
