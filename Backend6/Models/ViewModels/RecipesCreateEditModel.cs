2141d8f9cfd0fda2378acf5ee3290e51f16194e0
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2021-06-28 19:37:06 +0700

    Added recipes

---
+++ Backend6/Models/ViewModels/RecipesCreateEditModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rationality.Models.ViewModels
+{
+    public class RecipesCreateEditModel
+    {
+        public string Name { get; set; }
+        public int Kcal { get; set; }
+        public int Proteins { get; set; }
+        public int Fats { get; set; }
+        public int Carbohydrates { get; set; }
+        public double Price { get; set; }
+        public string CookingMethod { get; set; }
+        public Meal Meal { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
