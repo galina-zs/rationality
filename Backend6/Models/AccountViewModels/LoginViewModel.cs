efa707bcd8d275dff59adc19ba29c2ca6f1c31e7
Vadim Babikov <b.vadim.alexeyevich@gmail.com>
2021-06-28 22:38:18 +0700

    pull

--- Backend6/Models/AccountViewModels/LoginViewModel.cs
+++ Backend6/Models/AccountViewModels/LoginViewModel.cs
@@ -15,5 +15,8 @@ namespace Rationality.Models.AccountViewModels
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
